f55c61193478e42f1da5f10c0e2e8195feb6ab8e
Ryan Wong <ryan@rwmobi.com>
2024-03-25 18:18:15 +0000

    [feature] App: initial commit (up to chapter 11)

---
+++ JRNL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JRNL
+//
+//  Created by 🇭🇰Ry Wong on 19/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
